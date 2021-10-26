3f55f68e1c7b84f3ab21e98910cbae3f0cf1b5db
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-10-26 22:01:33 +0200

    Add Table component

---
+++ components/Table.js
@@ -0,0 +1,62 @@
+import { styled } from "stitches.config"
+
+const TableCellContainer = styled("td", {
+  textStyle: "$table-cell",
+  textAlign: "left",
+  p: "$small",
+})
+
+function TableCell({ children }) {
+  return <TableCellContainer>{children}</TableCellContainer>
+}
+
+const TableColumnHeaderContainer = styled("th", {
+  textStyle: "$table-head",
+  textAlign: "left",
+  p: "$small",
+  borderWidth: 0,
+  borderBottomWidth: 2,
+  borderBottomStyle: "solid",
+  borderColor: "$gray-300",
+})
+
+function TableColumnHeader({ children }) {
+  return <TableColumnHeaderContainer>{children}</TableColumnHeaderContainer>
+}
+
+function TableRow({ children }) {
+  return <tr>{children}</tr>
+}
+
+function TableBody({ children }) {
+  return <tbody>{children}</tbody>
+}
+
+function TableHeaderRow({ children }) {
+  return <tr>{children}</tr>
+}
+
+function TableHeader({ children }) {
+  return (
+    <thead>
+      <TableHeaderRow>{children}</TableHeaderRow>
+    </thead>
+  )
+}
+
+const TableContainer = styled("table", {
+  width: "100%",
+  borderCollapse: "collapse",
+})
+
+function Table(props) {
+  return <TableContainer>{props.children}</TableContainer>
+}
+
+Table.Header = TableHeader
+Table.Body = TableBody
+Table.Column = TableColumnHeader
+Table.Cell = TableCell
+Table.Row = TableRow
+
+export default Table

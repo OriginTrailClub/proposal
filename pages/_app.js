2198a1a991907e58eae489ebcd35eefe3068a973
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-10-26 20:25:41 +0200

    Add global styles with stitches wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,32 @@
-import '../styles/globals.css'
+import { globalCss } from 'stitches.config';
+
+const globalStyles = globalCss({
+  html: {
+    height: "100%",
+    width: "100%",
+  },
+  body: {
+    margin: 0,
+    minHeight: "100%",
+    width: "100%",
+    fontFamily: "$default",
+    color: "$text-body",
+    "-webkit-font-smoothing": "antialiased",
+    "-moz-osx-font-smoothing": "grayscale",
+  },
+  "#__next": {
+    minHeight: "100%",
+    width: "100%",
+  },
+  "*": {
+    boxSizing: "border-box",
+    position: "relative",
+  },
+})
 
 function MyApp({ Component, pageProps }) {
+  globalStyles();
+
   return <Component {...pageProps} />
 }
 

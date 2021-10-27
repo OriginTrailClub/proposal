233827e8062782cff9f863ec9f118a3f1a413466
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-10-27 22:14:59 +0200

    Add community video

---
+++ content/proposal/Video.js
@@ -0,0 +1,50 @@
+import { styled } from "stitches.config";
+
+const VideoContainer = styled("div", {
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+});
+
+const VideoLabel = styled("span", {
+  textAlign: "center",
+  textStyle: "$table-cell",
+  py: "$regular",
+});
+
+const VideoWrapper = styled("div", {
+  paddingBottom: "56.25%",
+  height: 0,
+  width: "100%",
+});
+
+const VideoIframe = styled("iframe", {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+});
+
+function Video() {
+  return (
+    <VideoContainer>
+      <VideoWrapper>
+        <VideoIframe
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/x2JGAfoa3wM?controls=0"
+          title="OriginTrail Community Hub Release Video"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </VideoWrapper>
+      <VideoLabel aria-hidden="true">
+        OriginTrail Community Hub Release Video
+      </VideoLabel>
+    </VideoContainer>
+  );
+}
+
+export default Video;

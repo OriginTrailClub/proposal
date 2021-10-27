08389ea76f6d8b85fbf36e2b95a94e0bb59595c8
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-10-27 20:49:57 +0200

    Wrap Card grid on mobile

--- components/Card/Grid.js
+++ components/Card/Grid.js
@@ -3,7 +3,11 @@ import { styled } from 'stitches.config';
 const GridContainer = styled('div', {
   display: 'grid',
   gridGap: '$regular',
-  gridTemplateColumns: '1fr 1fr'
+  gridTemplateColumns: '1fr',
+
+  '@bp1': {
+    gridTemplateColumns: '1fr 1fr'
+  }
 });
 
 function Grid({ children }) {

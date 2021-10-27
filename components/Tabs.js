aaef6101fecff86d6ea2db6fa32983bc767bd818
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-10-27 21:54:55 +0200

    Make tabs responsive

--- components/Tabs.js
+++ components/Tabs.js
@@ -2,41 +2,41 @@ import * as React from "react";
 
 import { styled } from "stitches.config";
 
-import { Item, useCollection } from '@react-stately/collections';
+import { Item, useCollection } from "@react-stately/collections";
 import { useTab, useTabList, useTabPanel } from "@react-aria/tabs";
-import { useTabListState } from '@react-stately/tabs';
-import { ListCollection } from '@react-stately/list';
+import { useTabListState } from "@react-stately/tabs";
+import { ListCollection } from "@react-stately/list";
 
 import useFindElementOfType from "hooks/useFindElementOfType";
 
-const TabItemContainer = styled('li', {
-  px: '$regular',
-  py: '$small',
-  textStyle: '$action',
-  color: '$text-subtle',
+const TabItemContainer = styled("li", {
+  px: "$regular",
+  py: "$small",
+  textStyle: "$action",
+  color: "$text-subtle",
   borderWidth: 1,
   borderBottomWidth: 2,
-  borderColor: 'transparent',
-  borderStyle: 'solid',
+  borderColor: "transparent",
+  borderStyle: "solid",
   marginBottom: -2,
   variants: {
     selected: {
       true: {
-        color: '$indigo-600',
-        borderColor: '$indigo-100',
-        backgroundColor: '$indigo-50',
-        '&:after': {
-          'content': '',
-          position: 'absolute',
+        color: "$indigo-600",
+        borderColor: "$indigo-100",
+        backgroundColor: "$indigo-50",
+        "&:after": {
+          content: "",
+          position: "absolute",
           bottom: -2,
           left: -1,
           right: -1,
           height: 2,
-          backgroundColor: '$indigo-600',
-        }
-      }
-    }
-  }
+          backgroundColor: "$indigo-600",
+        },
+      },
+    },
+  },
 });
 
 function TabItem({ item, state }) {
@@ -55,14 +55,21 @@ function TabItem({ item, state }) {
   );
 }
 
-const TabListContainer = styled('ul', {
-  listStyle: 'none',
-  display: 'flex',
+const TabListContainer = styled("div", {
+  overflowY: 'hidden',
+  overflowX: 'scoll',
+  pb: 1,
+  mb: '$large'
+});
+
+const TabListContainerItems = styled("ul", {
+  listStyle: "none",
+  display: "flex",
   margin: 0,
   padding: 0,
   borderBottomWidth: 1,
-  borderBottomStyle: 'solid',
-  borderBottomColor: '$gray-200',
+  borderBottomStyle: "solid",
+  borderBottomColor: "$gray-200",
 });
 
 function TabList({ state, ...props }) {
@@ -71,10 +78,12 @@ function TabList({ state, ...props }) {
   let { tabListProps } = useTabList(props, state, ref);
 
   return (
-    <TabListContainer ref={ref} {...tabListProps}>
-      {Array.from(state.collection).map(item => (
-        <TabItem key={item.key} item={item} state={state} />
-      ))}
+    <TabListContainer>
+      <TabListContainerItems ref={ref} {...tabListProps}>
+        {Array.from(state.collection).map((item) => (
+          <TabItem key={item.key} item={item} state={state} />
+        ))}
+      </TabListContainerItems>
     </TabListContainer>
   );
 }
@@ -88,29 +97,34 @@ function TabPanel({ state, ...props }) {
     <div {...tabPanelProps} ref={ref}>
       {props.children}
     </div>
-  )
+  );
 }
 
 function TabPanels({ state, ...props }) {
-  const factory = nodes => new ListCollection(nodes);
-  const collection = useCollection(props, factory, { suppressTextValueWarning: true });
+  const factory = (nodes) => new ListCollection(nodes);
+  const collection = useCollection(props, factory, {
+    suppressTextValueWarning: true,
+  });
 
   const selectedItem = collection.getItem(state.selectedKey);
 
   return (
     <div>
       {selectedItem != null ? (
-        <TabPanel {...selectedItem?.props} state={state} key={selectedItem.key} />
+        <TabPanel
+          {...selectedItem?.props}
+          state={state}
+          key={selectedItem.key}
+        />
       ) : undefined}
     </div>
   );
 }
 
-const TabsContainer = styled('div', {
-  display: 'flex',
-  width: '100%',
-  gap: '$large',
-  flexDirection: 'column',
+const TabsContainer = styled("div", {
+  display: "flex",
+  width: "100%",
+  flexDirection: "column",
 });
 
 function Tabs(props) {
@@ -126,7 +140,7 @@ function Tabs(props) {
       {React.cloneElement(TabList, { state })}
       {React.cloneElement(TabPanels, { state })}
     </TabsContainer>
-  )
+  );
 }
 
 Tabs.List = TabList;
@@ -134,4 +148,4 @@ Tabs.Item = Item;
 Tabs.Panels = TabPanels;
 Tabs.Panel = Item;
 
-export default Tabs
+export default Tabs;
